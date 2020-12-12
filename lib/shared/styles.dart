0c872cb5741356a80a3d1975a08c3a220b5035cd
lenargum <lenargamgam@gmail.com>
2020-12-13 01:54:01 +0300

    changed InputDecoration style on similar to designed one

--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -5,10 +5,10 @@ const textInputDecoration = InputDecoration(
   filled: true,
   contentPadding: EdgeInsets.all(12.0),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.white, width: 2.0),
+    borderSide: BorderSide(color: Color.fromARGB(30, 0, 0, 0), width: 2.0),
   ),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.pink, width: 2.0),
+    borderSide: BorderSide(color: ColorsStyle.primary, width: 2.0),
   ),
 );
 
