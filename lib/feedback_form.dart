f88db74811998a667c5c4982b17b32a3174432e4
lenargum <lenargamgam@gmail.com>
2020-12-14 21:21:34 +0300

    remade handleFeedbackLongPress with BottomSheet

--- lib/feedback_form.dart
+++ lib/feedback_form.dart
@@ -26,11 +26,11 @@ class _FeedbackFormState extends State<FeedbackForm> {
     email = FirebaseAuth.instance.currentUser.email;
   }
 
-  handleSubmitFeedback() {
+  handleSubmitFeedback() async {
     // TODO: show a confirmation message (ex: Are you sure?)
     StudentFeedback f = new StudentFeedback('', widget.taCourse.taId,
         widget.taCourse.courseId, controller.text, uid, email, [], []);
-    submitFeedback(f, isAnonymous);
+    await submitFeedback(f, isAnonymous);
     controller.text = '';
     setState(() {
       isAnonymous = false;
@@ -138,14 +138,6 @@ class _FeedbackDisplayState extends State<FeedbackDisplay> {
     return result;
   }
 
-  @override
-  void initState() {
-    super.initState();
-    uid = FirebaseAuth.instance.currentUser.uid;
-    email = FirebaseAuth.instance.currentUser.email;
-    fetchFeedback();
-  }
-
   handleUpvote(StudentFeedback f) {
     // toggle the upvote state
     if (f.upvotes.contains(email))
@@ -168,85 +160,234 @@ class _FeedbackDisplayState extends State<FeedbackDisplay> {
     return updateVotes(f);
   }
 
-  handleFeedbackLongPress(StudentFeedback f) async {
-    bool shouldDelete = await showDialog<bool>(
-          context: context,
-          barrierDismissible: true,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Delete feedback'),
-              content: Text('Are you sure you want to delete this feedback?'),
-              actions: [
-                TextButton(
-                  child: Text('Delete'),
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                  },
-                ),
-                TextButton(
-                  child: Text('Cancel'),
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                ),
-              ],
-            );
-          },
-        ) ??
+  Future<bool> areYouSure(String title, String message) async {
+    return await showDialog<bool>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+          ],
+        );
+      },
+    ) ??
         false;
+  }
 
-    if (shouldDelete) deleteFeedback(f);
+  handleFeedbackLongPress(BuildContext context, StudentFeedback f) async {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext bottomSheetContext) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Delete feedback'),
+              onTap: () async {
+                var itemType = "Feedback";
+                var representation;
+                if (f.message.length < 10) {
+                  representation = f.message.substring(0, f.message.length);
+                } else {
+                  representation = f.message.substring(0, 10) + "...";
+                }
+                try {
+                  if (await areYouSure("Delete feedback",
+                      "Are you sure you want to delete this feedback?")) {
+                    await deleteFeedback(f);
+                    showSuccessSnackBar(context,
+                        "$itemType '$representation' successfully deleted!");
+                  }
+                } catch (e, p) {
+                  print(e.toString() + ' ' + p.toString());
+                  if (e.toString().contains("] ")) {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType '$representation'",
+                        e.toString().split("] ")[1]);
+                  } else {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType '$representation'",
+                        e.toString());
+                  }
+                }
+                await fetchFeedback();
+                Navigator.pop(bottomSheetContext);
+              },
+            ),
+            (isAdmin)
+                ? ListTile(
+              leading: Icon(Icons.delete_sweep),
+              title: Text('Delete all from user'),
+              onTap: () async {
+                var itemType = "Feedback";
+                Student student;
+                try {
+                  student = await getStudentById(f.uid);
+                  if (await areYouSure("Delete all feedback from user",
+                      "Are you sure you want to delete all feedback left here from this user?")) {
+                    await deleteFeedbackByStudentInTaCourse(
+                        f.uid, f.courseId, f.taId);
+                    showSuccessSnackBar(context,
+                        "All $itemType from ${student
+                            .name} successfully deleted!");
+                  }
+                } catch (e, p) {
+                  print(e.toString() + ' ' + p.toString());
+                  if (e.toString().contains("] ")) {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType from ${student.name ??
+                            "User"}.",
+                        e.toString().split("] ")[1]);
+                  } else {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType from ${student.name ??
+                            "User"}.",
+                        e.toString());
+                  }
+                }
+                await fetchFeedback();
+                Navigator.pop(context);
+              },
+            )
+                : Container(),
+            (isAdmin)
+                ? ListTile(
+              leading: Icon(Icons.person_remove),
+              title: Text('Delete user'),
+              onTap: () async {
+                var itemType = "User";
+                Student student;
+                try {
+                  student = await getStudentById(f.uid);
+                  if (f.uid == _auth.getCurrentUserId()) {
+                    throw Exception("You can't delete yourself");
+                  }
+                  if (await areYouSure("Delete user",
+                      "Are you sure you want to delete this user and all related feedback?")) {
+                    await deleteStudent(f.uid);
+                    showSuccessSnackBar(context,
+                        "$itemType '${student.name}' successfully deleted!");
+                  }
+                } catch (e, p) {
+                  print(e.toString() + ' ' + p.toString());
+                  if (e.toString().contains("] ")) {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType '${student.name ??
+                            "username"}'.",
+                        e.toString().split("] ")[1]);
+                  } else {
+                    showErrorSnackBar(
+                        context,
+                        "Unable to delete $itemType '${student.name ??
+                            "username"}'.",
+                        e.toString());
+                  }
+                }
+                await fetchFeedback();
+                Navigator.pop(context);
+              },
+            )
+                : Container(),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    uid = FirebaseAuth.instance.currentUser.uid;
+    email = FirebaseAuth.instance.currentUser.email;
+    fetchFeedback();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-        itemCount: feedbackList.length,
-        itemBuilder: (context, index) {
-          var f = feedbackList[index];
-          bool upvoted = f.upvotes.contains(email);
-          bool downvoted = f.downvotes.contains(email);
-          // TODO: display a negative or toxic warning depending on the sentiment of the feedback
-          return ListTile(
-            title: Text(f.email),
-            subtitle: Text(f.message),
-            onTap: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => FeedbackPage(f))),
-            onLongPress:
-                uid == f.uid ? () => handleFeedbackLongPress(f) : null,
-            leading: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: upvoted ? Colors.yellow : null),
-              constraints: BoxConstraints(maxWidth: 72),
-              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                Text(f.upvotes.length.toString()),
-                IconButton(
-                  icon: Icon(Icons.arrow_upward),
-                  onPressed: () => handleUpvote(f),
-                )
-              ]),
-            ),
-            trailing: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: downvoted ? Colors.red[200] : null),
-              constraints: BoxConstraints(maxWidth: 72),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(f.downvotes.length.toString()),
-                  IconButton(
-                    icon: Icon(Icons.arrow_downward),
-                    onPressed: () => handleDownvote(f),
+      child: FutureBuilder<Student>(
+          future: getStudentById(uid),
+          builder: (futureContext, snapshot) {
+            if (snapshot.hasData) {
+              isAdmin = snapshot.data.isAdmin();
+            }
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              showErrorSnackBar(context, "Failed fetching user role", "");
+            }
+
+            return ListView.builder(
+              itemCount: feedbackList.length,
+              itemBuilder: (listContext, index) {
+                var f = feedbackList[index];
+                bool upvoted = f.upvotes.contains(email);
+                bool downvoted = f.downvotes.contains(email);
+                // TODO: display a negative or toxic warning depending on the sentiment of the feedback
+                return ListTile(
+                  title: Text(f.email),
+                  subtitle: Text(f.message),
+                  onTap: () =>
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) => FeedbackPage(f))),
+                  onLongPress: (f.uid == uid || isAdmin)
+                      ? () => handleFeedbackLongPress(context, f)
+                      : null,
+                  leading: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: upvoted ? Colors.yellow : null),
+                    constraints: BoxConstraints(maxWidth: 72),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(f.upvotes.length.toString()),
+                          IconButton(
+                            icon: Icon(Icons.arrow_upward),
+                            onPressed: () => handleUpvote(f),
+                          )
+                        ]),
                   ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+                  trailing: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: downvoted ? Colors.red[200] : null),
+                    constraints: BoxConstraints(maxWidth: 72),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(f.downvotes.length.toString()),
+                        IconButton(
+                          icon: Icon(Icons.arrow_downward),
+                          onPressed: () => handleDownvote(f),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
