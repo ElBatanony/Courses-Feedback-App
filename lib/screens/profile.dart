e2244de969de479ac1612f6b3a27ed1e51291196
Kostenko Nikita <n.kostenko@x5.ru>
2020-12-14 21:47:12 +0300

    finalizing profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:innopolis_feedback/data.dart';
+import 'package:innopolis_feedback/services/auth.dart';
 import 'package:innopolis_feedback/services/database.dart';
 import 'package:innopolis_feedback/shared/bottom_navbar.dart';
 import 'package:innopolis_feedback/shared/loading.dart';
 import 'package:innopolis_feedback/ta_profile_page.dart';
+import 'package:innopolis_feedback/ui/action_button.dart';
 import 'package:innopolis_feedback/ui/app_bar.dart';
 import 'package:innopolis_feedback/ui/list_item.dart';
 
@@ -17,20 +19,27 @@ class _ProfileState extends State<Profile> {
   User user;
   Student student;
   bool loading = true;
+  List<TA> tas = [];
 
   getStudent() async {
-    student = await DatabaseService(user.uid).student;
+    Student tempStudent = await DatabaseService(user.uid).student;
+
+    List<TA> tempTas = await Future.wait(
+        tempStudent.favoriteTAs.map((taId) => getTaById(taId)));
+
     setState(() {
       loading = false;
+      tas = tempTas;
+      student = tempStudent;
     });
   }
 
-  taToWidget(TA ta)  => ListItem(
-        title: ta.name,
-        onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => TaProfilePage(ta.id, ta.name))));
+  taToWidget(TA ta) => ListItem(
+      title: ta.name,
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => TaProfilePage(ta.id, ta.name))));
 
   @override
   void initState() {
@@ -44,27 +53,24 @@ class _ProfileState extends State<Profile> {
     return loading
         ? Loading()
         : Scaffold(
-            appBar:
-                CustomAppBar(title: student.name),
+            appBar: CustomAppBar(
+              title: student.name,
+              goBackIconVisible: true,
+              onGoBack: () => Navigator.pop(context),
+            ),
             body: Center(
-                child:
-                    Column(
-                      children: [
-                        Text('You can visit one of your favorite TAs:'),
-                        ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: student.favoriteTAs.length,
-                            itemBuilder: (context, index) {
-                              final taId = student.favoriteTAs[index];
-                              return FutureBuilder(
-                                  future: getTaById(taId),
-                                  builder: (context, snapshot) =>
-                                      taToWidget(snapshot.data));
-                            }),
-                      ],
-                    )
+                child: Column(
+              children: [
+                Text('You can visit one of your favorite TAs:'),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: tas.length,
+                      itemBuilder: (context, index) => taToWidget(tas[index])),
                 ),
+                ActionButton(
+                    text: "Sign out?", onPressed: AuthService().signOut)
+              ],
+            )),
             bottomNavigationBar: BottomNavBar(),
           );
   }
